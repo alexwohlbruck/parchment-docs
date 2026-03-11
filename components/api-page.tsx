d67af406fc30cddf5cb33a57b70c819e73694d47
Alex Wohlbruck <alexwohlbruck@gmail.com>
2026-03-11 08:58:45 -0400

    Use env var for openAPI url

--- components/api-page.tsx
+++ components/api-page.tsx
@@ -2,5 +2,12 @@ import { APIPage as BaseAPIPage, type ApiPageProps } from 'fumadocs-openapi/ui'
 import { openapi } from '@/lib/openapi'
 
 export function APIPage(props: ApiPageProps) {
-  return <BaseAPIPage {...openapi.getAPIPageProps(props)} />
+  // Use OPENAPI_SPEC_URL at build time (e.g. on Netlify) when the spec can't be fetched from localhost
+  const document =
+    process.env.OPENAPI_SPEC_URL ?? props.document
+  return (
+    <BaseAPIPage
+      {...openapi.getAPIPageProps({ ...props, document })}
+    />
+  )
 }
